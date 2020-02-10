7fbcb25d9af81356e73bcac39cb50a4f09ad38d6
ShivamKumarJha <jha.shivam3@gmail.com>
2020-02-10 20:28:52 +0000

    capricorn-user-8.0.0-OPR1.170623.032-V11.0.2.0.OAGCNXM-release-keys
    Signed-off-by: ShivamKumarJha <jha.shivam3@gmail.com>

--- lineage_capricorn.mk
+++ lineage_capricorn.mk
@@ -17,7 +17,7 @@ PRODUCT_MODEL := MI 5s
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := capricorn
-PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="capricorn-user 8.0.0 OPR1.170623.032 V10.2.3.0.OAGMIXM release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="capricorn-user 8.0.0 OPR1.170623.032 V11.0.2.0.OAGCNXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := Xiaomi/capricorn/capricorn:8.0.0/OPR1.170623.032/V10.2.3.0.OAGMIXM:user/release-keys
+BUILD_FINGERPRINT := Xiaomi/capricorn/capricorn:8.0.0/OPR1.170623.032/V11.0.2.0.OAGCNXM:user/release-keys
--- vendor_prop.mk
+++
@@ -1,3 +0,0 @@
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=440 \
-    ro.lineage.build.vendor_security_patch=2018-10-01 \
